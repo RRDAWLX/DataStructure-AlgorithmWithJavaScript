34fa79b8ac7c2ea9909376c0e6c75357218b2062
longxiang.wang <longxiang.wang@vipshop.com>
2018-03-07 18:45:57 +0800

    使用mocha做单元测试

--- test/sort.js
+++ test/sort.js
@@ -1,13 +1,21 @@
+const assert = require('assert');
 const {quickSort} = require('../algorithms/sort/index.js');
 
 let arr = [];
 for(var i = 0; i < 10; i++){
 	arr[i] = Math.floor(Math.random() * 100);
 }
+let sortedArr = arr.slice();
+sortedArr.sort((a, b) => {
+	return a - b;
+});
 
-let arr2 = [];
-console.log('initial: ', arr);
-
-arr2 = arr.slice();
-quickSort(arr2);
-console.log('quick sort: ', arr2);
+describe('Sort', () => {
+	describe('.quickSort()', () => {
+		it(`should sort the array as [${sortedArr}].`, () => {
+			let testArr = arr.slice();
+			quickSort(testArr);
+			assert.deepEqual(testArr, sortedArr);
+		})
+	});
+});
