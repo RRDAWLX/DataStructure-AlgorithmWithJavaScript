0450a37961968fc4f12d57bf4cbecb7b8ee7de7c
rrdawlx <815311850@qq.com>
2020-05-03 18:09:04 +0800

    添加支持加、减、乘、除、括号运算符的数学表达式计算器。

--- test/work.js
+++ test/work.js
@@ -1,7 +1,44 @@
 const assert = require('assert')
+const calculator = require('../work/calculator')
 const mergeJointIntervals = require('../work/merge-joint-intervals')
 
 describe('Work', () => {
+  describe('calculator', () => {
+    let expressions = [
+      {
+        str: '1+2×3÷(10-4)',
+        infix: [1, '+', 2, '×', 3, '÷', '(', 10, '-', 4, ')'],
+        postfix: [1, 2, 3, '×', 10, 4, '-', '÷', '+'],
+        result: 2,
+      },
+    ]
+
+    for (let i = 0, len = expressions.length; i < len; i++) {
+      let expression = expressions[i]
+      let infixResult = JSON.stringify(calculator.parseIntoInfix(expression.str))
+      let expectedInfix = JSON.stringify(expression.infix)
+      it(`infix result ${infixResult} should be ${expectedInfix}: `, () => {
+        assert.equal(infixResult, expectedInfix)
+      })
+
+      let postfixResult = JSON.stringify(calculator.tranformInfixToPostfix(expression.infix))
+      let expectedPostfix = JSON.stringify(expression.postfix)
+      it(`postfix result ${postfixResult} should be ${expectedPostfix}: `, () => {
+        assert.equal(postfixResult, expectedPostfix)
+      })
+
+      let calculateResult = calculator.calculate(expression.postfix)
+      it(`calculate result ${calculateResult} should be ${expression.result}: `, () => {
+        assert.equal(calculateResult, expression.result)
+      })
+      
+      let calcResult = calculator.calc(expression.str)
+      it(`calc result ${calcResult} should be ${expression.result}: `, () => {
+        assert.equal(calcResult, expression.result)
+      })
+    }
+  })
+
   let intervals = [[1, 4], [5, 8], [3, 6], [10, 14], [10, 11], [18, 23]]
   let targetIntervals = [[1, 8], [10, 14], [18, 23]]
   describe(`merge joint intervals: ${JSON.stringify(intervals)}`, () => {
---
+++ work/calculator.js
@@ -0,0 +1,121 @@
+const operators = [
+  { operator: '+', priority: 0 },
+  { operator: '-', priority: 0 },
+  { operator: '×', priority: 1 },
+  { operator: '÷', priority: 1 },
+  { operator: '(', priority: 2 },
+  { operator: ')', priority: 2 },
+]
+const priorities = operators.reduce((obj, item) => {
+  obj[item.operator] = item.priority
+  return obj
+}, {})
+
+function parseIntoInfix (expression) {
+  let infix = []
+  let num = ''
+
+  for (let i = 0, len = expression.length; i < len; i++) {
+    let charCode = expression.charCodeAt(i)
+
+    // 数字 code 在 48 至 57 之间，小数点的 code 为 46。
+    if ((charCode >= 48 && charCode <= 57) || charCode === 46) {
+      num += expression[i]
+    } else {
+      num && infix.push(+num)
+      infix.push(expression[i])
+      num = ''
+    }
+  }
+
+  num && infix.push(+num)
+  return infix
+}
+
+function tranformInfixToPostfix (infix) {
+  let postfix = []
+  let operators = []
+  
+  for (let i = 0, len = infix.length; i < len; i++) {
+    let slice = infix[i]
+
+    if (typeof slice === 'number') {
+      postfix.push(slice)
+    } else {
+      if (slice === ')') {
+        while (operators.length) {
+          let operator = operators.pop()
+
+          if (operator === '(') {
+            break
+          } else {
+            postfix.push(operator)
+          }
+        }  
+      } else {
+        while (operators.length) {
+          let operator = operators.pop()
+
+          if (operator === '(' || priorities[operator] < priorities[slice]) {
+            operators.push(operator)
+            break
+          } else {
+            postfix.push(operator)
+          }
+        }
+
+        operators.push(slice)
+      }
+    }
+  }
+
+  while (operators.length) {
+    postfix.push(operators.pop())
+  }
+
+  return postfix
+}
+
+function calculate (postfix) {
+  let stack = []
+
+  for (let i = 0, len = postfix.length; i < len; i++) {
+    let slice = postfix[i]
+
+    if (typeof slice === 'number') {
+      stack.push(slice)
+    } else {
+      let num2 = stack.pop()
+      let num1 = stack.pop()
+
+      switch (slice) {
+        case '+':
+          stack.push(num1 + num2)
+          break;
+        
+        case '-':
+          stack.push(num1 - num2)
+          break;
+        
+        case '×':
+          stack.push(num1 * num2)
+          break;
+        
+        case '÷':
+          stack.push(num1 / num2)
+          break;
+      }
+    }
+  }
+
+  return stack.pop()
+}
+
+function calc (expression) {
+  return calculate(tranformInfixToPostfix(parseIntoInfix(expression)))
+}
+
+module.exports.parseIntoInfix = parseIntoInfix
+module.exports.tranformInfixToPostfix = tranformInfixToPostfix
+module.exports.calculate = calculate
+module.exports.calc = calc
